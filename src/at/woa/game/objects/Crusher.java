39322e84d28629dd692f9d6865400be54ad20d1e
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2024-02-28 10:16:44 +0100

    commit

---
+++ src/at/woa/game/objects/Crusher.java
@@ -0,0 +1,32 @@
+package at.woa.game.objects;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+public class Crusher extends SpielObjekt{
+    private Input input;
+    private Rectangle shape;
+    public Crusher(int x, int y, Image image,Input input) {
+        super(x, y, image);
+        this.input = input;
+        shape = new Rectangle(x,y, image.getWidth(), image.getHeight());
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public Shape getShape() {
+        return null;
+    }
+
+    @Override
+    public void update(int delta) {
+
+    }
+}
